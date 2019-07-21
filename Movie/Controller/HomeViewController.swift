8ba3f08ac4a836862541f3ab39210f910effaff7
Yashar <vb_syntax@yahoo.com>
2019-07-21 18:33:40 +0430

    change homeview controller

---
+++ Movie/Controller/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  Movie
+//
+//  Created by Yashar on 7/21/19.
+//  Copyright © 2019 Yashar Azabdaftari. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    // ===========================
+    // MARK: - Poperties
+    // ===========================
+    @IBOutlet weak var tableView: UITableView!{
+        didSet {
+            
+        }
+    }
+    // ===========================
+    // MARK: - Life Cycle
+    // ===========================
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
+private extension HomeViewController {
+    
+}
